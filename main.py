8654e7091de151eff25af53686bf4609886abd99
rmc8 <kmyashi@rmc-8.com>
2021-02-05 21:17:28 +0900

    [ADD] Jacket scraper

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import re
 import os
+from time import sleep
 import pathlib
+from typing import List, Tuple, Optional
+
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-def get_cd_info(path):
+def get_cd_info(path: str) -> Tuple[str, str, str]:
     """
     Search to the lowest directory and return the name of the CD, the name of the artist and its file path.
     Args:
@@ -18,9 +26,46 @@ def get_cd_info(path):
             get_cd_info(f"{path}\\{dir}")
 
 
+def web_driver(headless: bool = True) -> webdriver:
+    options = Options()
+    options.add_argument(f"log-level=3")
+    if headless:
+        options.add_argument("--headless")
+    return webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+
+def get_asin(url_list: str) -> Optional[str]:
+    for url in url_list:
+        match = re.findall(r"^(?:https://.*([0-9B]{1}[0-9A-Z]{9}))", str(url), flags=re.VERBOSE)
+        if match:
+            return match[0]
+    return None
+
+
 def main():
-    for path, artist, name in get_cd_info(r"E:\Music\HiRes"):
-        print(path, artist, name)
+    driver = web_driver()
+    try:
+        for path, artist, name in get_cd_info(r"E:\Music\01oldLinux\Afterglow"):
+            img_path = f"{path}\\{name}.jpg"
+            if os.path.exists(img_path):
+                continue
+            sleep(1)
+            url = f"https://www.google.com/search?q={artist}+{name}+amazon"
+            driver.get(url)
+            urls = [e.get_attribute("href")
+                    for e in driver.find_elements_by_tag_name("a")]
+            asin = get_asin(urls)
+            if asin is None:
+                continue
+            img_url = f"http://images-jp.amazon.com/images/P/{asin}.6.LZZZZZZZ.jpg"
+            res = requests.get(img_url)
+            with open(img_path, "wb") as img_file:
+                img_file.write(res.content)
+            if os.path.getsize(img_path) < 100:
+                os.remove(img_path)
+                        
+    finally:
+        driver.quit()
 
 
 if __name__ == "__main__":
